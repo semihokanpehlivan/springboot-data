437c23f9b771754b4acba8916b70434c8fa98b9b
semihokanpehlivan <semihokanp@gmail.com>
2015-11-01 23:18:53 +0300

    Added the @Scheduled annotation

--- src/main/java/com/example/HibernateMainApplication.java
+++ src/main/java/com/example/HibernateMainApplication.java
@@ -5,11 +5,13 @@ import com.example.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author Semih Okan Pehlivan.
  */
 @SpringBootApplication
+@EnableScheduling
 public class HibernateMainApplication {
 
     public static void main(String[] args) {
@@ -17,7 +19,7 @@ public class HibernateMainApplication {
         ConfigurableApplicationContext context = springApplication.run(args);
         UserRepository userRepository = context.getBean("user-repo", UserRepository.class);
         userRepository.addUser(new User("SOP"));
-        
+
         for (User u : userRepository.findUserByName("Okan")) {
             System.out.println(u.getId());
             System.out.println(u.getName());
--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.example.repository;
 
 import com.example.models.User;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate4.HibernateTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +25,8 @@ public class UserRepository {
     @Autowired
     HibernateTemplate hibernateTemplate;
 
+    private static Logger logger = LoggerFactory.getLogger(UserRepository.class.getName());
+
     public void addUser(User user) {
         sessionFactory.getCurrentSession().save(user);
     }
@@ -34,6 +39,14 @@ public class UserRepository {
     public User getUserById(long id) {
         User user = hibernateTemplate.get(User.class,id);
         return user;
+    }
+
+    @Scheduled(cron = "0,30 * * * * *")
+    public void cron() {
+        logger.info(">>> cron");
+        List<User> list = hibernateTemplate.loadAll(User.class);
+        logger.info("There are {} user in database",list.size());
+        logger.info("<<< cron");
     }
 
 }
