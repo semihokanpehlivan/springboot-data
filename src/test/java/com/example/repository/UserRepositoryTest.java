3e35b75d1c4d492f175c532a3bf1863a6ace42ef
semihokanpehlivan <semihokanp@gmail.com>
2015-11-01 13:25:13 +0300

    Added the output capture and assertThat method

--- src/test/java/com/example/repository/UserRepositoryTest.java
+++ src/test/java/com/example/repository/UserRepositoryTest.java
@@ -3,9 +3,12 @@ package com.example.repository;
 import com.example.AbstractTest;
 import com.example.models.User;
 import org.apache.tomcat.jdbc.pool.DataSource;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.OutputCapture;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -20,11 +23,16 @@ public class UserRepositoryTest extends AbstractTest{
     @Autowired
     DataSource dataSource;
 
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
+
     @Test
     public void addUserTest() {
         User user = new User("User");
         userRepository.addUser(user);
 
+        System.out.println("Hello Spring1");
+        Assert.assertThat(outputCapture.toString(), CoreMatchers.containsString("Spring"));
         Assert.assertNotNull(dataSource);
     }
 
