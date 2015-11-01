59766bdbda17ec0486f9b21906c000a03af42038
semihokanpehlivan <semihokanp@gmail.com>
2015-11-01 18:34:52 +0300

    Added the RestTemplateTest

---
+++ src/test/java/com/example/repository/RestTemplateTest.java
@@ -0,0 +1,23 @@
+package com.example.repository;
+
+import com.example.AbstractTest;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+
+/**
+ * @author Semih Okan Pehlivan.
+ */
+public class RestTemplateTest extends AbstractTest{
+
+    public TestRestTemplate testRestTemplate = new TestRestTemplate();
+
+    @Test
+    public void testRequest() {
+        HttpHeaders headers = testRestTemplate.getForEntity("https://www.google.com.tr", String.class).getHeaders();
+        Assert.assertNotNull(headers);
+    }
+
+}
